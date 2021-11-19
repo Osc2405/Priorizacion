bafdad9ff5d6f39875ef8cfce9fac6ddc6e08190
Osc2405 <orosero2405@gmail.com>
2021-11-19 09:46:02 -0500

    Se agregaron las tablas que se pueden modificar

--- priorizar.py
+++ priorizar.py
@@ -110,33 +110,38 @@ def Procesar():
     columnas_originales=df[["Nombre","Descripcion adicional","Marca","Modelo","Estado actual","Servicio","Clasificación biomedica","Clasificación de riesgo", "Cantidad de correctivos registrados"]]
     df_salida=columnas_originales.copy()
     df_salida["Estado ponderado"]=np.where(df_salida["Estado actual"]=="Activo",1,np.where(df_salida["Estado actual"]=="Fuera de servicio",1,0))
-    path2="datos/servicios.csv"
-    df_servicios=pd.read_csv(path2,sep=";",encoding='latin-1')
+    path2="datos/servicios.xlsx"
+    df_servicios=pd.read_excel(path2)
     servicios_dic = df_servicios.set_index('Servicio').to_dict()["Peso"]
     df_salida["Servicio ponderado"]=df_salida["Servicio"].apply(lambda x: servicios_dic[x])
 
-    clasificacion_dic={"TRATAMIENTO Y MANTENIMIENTO DE LA VIDA":3,
-                    "RAHABILITACIÓN":2,"DIAGNOSTICO":2,
-                    "APOYO":1,"N/R":1,"ANÁLISIS DE LABORATORIO":1,"REHABILITACIÓN":1,"NaN":1}
-    riesgo_dic={"I":1,"IIA":2,"IIB":3,"III":4,"NaN":1,"N/R":1}
+    path3="datos/clasificacion_biomedica.xlsx"
+    df_clasificacion=pd.read_excel(path3)
+    path4="datos/clasificacion_de_riesgo.xlsx"
+    df_riesgo=pd.read_excel(path4)
+    clasificacion_dic = df_clasificacion.set_index('Clasificacion_biomedica').to_dict()["Peso"]
+    riesgo_dic = df_riesgo.set_index('Clasificacion_de_riesgo').to_dict()["Peso"]
+    clasificacion_dic["NaN"]=1
+    riesgo_dic["NaN"]=1
 
-    df_salida["Clasificación biomedica"]=df_salida["Clasificación biomedica"].fillna("NaN")
-    df_salida["Clasificación de riesgo"]=df_salida["Clasificación de riesgo"].fillna("NaN")
+    df_salida["Clasificación biomedica"]=df_salida["Clasificación biomedica"].replace(np.nan,"NaN")
+    df_salida["Clasificación de riesgo"]=df_salida["Clasificación de riesgo"].replace(np.nan,"NaN")
     df_salida["Clasificacion ponderada"]=df_salida["Clasificación biomedica"].apply(lambda x: clasificacion_dic[x])
     df_salida["Riesgo ponderado"]=df_salida["Clasificación de riesgo"].apply(lambda x: riesgo_dic[x])
 
     
 
     df_salida["Correctivos ponderados"]=df_salida["Cantidad de correctivos registrados"].apply(func)
-    df_salida["Impacto operacional"]=1
+    
+    path5="datos/impacto_operacional.xlsx"
+    df_impacto=pd.read_excel(path5)
+    impacto_dic = df_impacto.set_index('Equipo').to_dict()["Peso"]
+    df_salida["Nombre"]=df_salida["Nombre"].str.upper()
+    df_salida["Impacto operacional"]=df_salida["Nombre"].apply(lambda x: impacto_dic[x])
+
     df_salida["Solicitud por el personal"]=1
 
 
-    # def total(x):
-    #     salida=x[9]*(x[10]*0.211+x[11]*0.033+x[12]*0.128+x[13]*0.06+x[14]*0.208+x[15]*0.359)
-    #     return salida
-
-    # df_salida["Total"]=df_salida.apply(total,axis=1)
     df_salida.to_excel("Datos_filtrados.xlsx")
 
     wbk=openpyxl.load_workbook("Datos_filtrados.xlsx")
@@ -147,13 +152,14 @@ def Procesar():
     wbk=openpyxl.load_workbook("Datos_filtrados.xlsx")
     wks=wbk.worksheets[0]
     maxRow=wks.max_row
-    for row in range(2,maxRow):
+    for row in range(2,maxRow+1):
         wks.cell(row=row,column=18).value="=K{}*(L{}*0.211+M{}*0.033+N{}*0.128+O{}*0.06+P{}*0.208+Q{}*0.359)".format(row,row,row,row,row,row,row)
     wbk.save("Datos_filtrados.xlsx")
     wbk.close()
 
 
-    print ("Hola")
+    messagebox.showinfo(message="Se ha completado la exportacion del archivo", title="Completado")
+    Limpiar()
 
 
 tabla = ttk.Treeview(frame1 , height=10)
